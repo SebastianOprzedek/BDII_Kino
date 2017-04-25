9d9081bc92bab83f58fde586d031b780b7778aaa
edytahareza <edytahareza@gmail.com>
2017-04-26 00:08:34 +0200

    skrypty do filmow i gatunkow

---
+++ skrypty/insert_filmy.sql
@@ -0,0 +1,144 @@
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(1, 'Historia zycia Forresta, chlopca o niskim ilorazie inteligencji z 
+  niedowladem konczyn, ktory staje sie miliarderem i bohaterem wojny w 
+  Wietnamie.', 'Forrest Gump', 1994, 142, 1);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(2, 'Czterech przyjaciol spedza wieczor kawalerski w Las Vegas. Nastepnego
+  dnia okazuje sie, ze zgubili pana mlodego i nic nie pamietaja.', 'Kac Vegas',
+  2009, 90, 1);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(3, 'Rok 2044, zostaja odhibernowani dwaj ostatni przedstawiciele plci
+  meskiej. Wlada jest w rekach Ligii Kobiet, ktora nie zamierza z niej 
+  zrezygnowac.', 'Seksmisja', 1983, 116, 1);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(4, 'Jack podejmuje prace stroza odcietego od swiata hotelu Overlook. 
+  Wkrotce idylla zamienia sie w koszmar.', 'Lsnienie', 1980, 146, 2);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(5, 'Slynna para badaczy zjawisk paranormalnych zostaje poproszona o pomoc
+  rodzinie, ktora terroryzuje potezny demon.', 'Obecnosc', 2013, 112, 2);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(6, 'Trojka studentow wyrusza z kamera w glab lasu w poszukiwaniu sladow 
+  przerazajacej wiedzmy.', 'Blair Witch Project', 1999, 81, 2);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(7, 'Targany nieslusznymi wyrzutami sumienia po smierci ojca maly lew 
+  Simba skazuje sie na wygnanie rezygnujac z przynaleznego mu miejsca na czele 
+  stada.', 'Krol Lew', 1994, 89, 3);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(8, 'Zaklecie wrozki zamienia okrutnego ksiecia w przerazajaca Bestie. 
+  Aby zdjac urok, Bestia musi zdobyc milosc pieknej Belli.', 'Piekna i Bestia',
+  1991, 90, 3);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(9, 'Aby ratowac ojca przed smiercia na wojnie, Mulan ucieka z domu i 
+  przebrana za mezczyzne szkoli sie w armii.', 'Mulan', 1998, 86, 3);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(10, 'Harry Potter musi wzi¹æ udzia w Turnieju Trójmagicznym, kiedy jego 
+  nazwisko zostaje wybrane przez tajemnicza Czare Ognia.', 'Harry Potter i 
+  Czara Ognia', 2005, 157, 4);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(11, 'Dziewietnastoletnia Alicja wraca do krainy z dziecinstwa, gdzie 
+  dowiaduje sie, ze jej przeznaczeniem jest zdetronizowac Krolowa Kier.', 
+  'Alicja w Krainie Czarow', 2010, 108, 4);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(12, 'Ubogi Charlie Bucket znajduje jeden z pieciu zlotych biletow, ktory
+  umozliwia mu zwiedzenie ogromnej fabryki czekolady.', 'Charlie i fabryka 
+  czekolady', 2005, 115, 4);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(13, 'Emerytowany straznik wiezienny opowiada przyjaciolce o niezwyklym 
+  mezczyznie, ktorego skazano na smierc za zabojstwo dwoch 9-letnich 
+  dziewczynek.', 'Zielona mila', 1999, 188, 5);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(14, 'Adaptacja opowiadania Stephena Kinga. Historia nieslusznie skazanego 
+  na dozywocie bankiera, ktory musi przezyc w brutalnym swiecie rzadzonym przez 
+  straznikow i wspolwiezniow', 'Skazani na Shawshank', 1994, 142, 5);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(15, 'Platny morderca ratuje dwunastoletnia dziewczynke, ktorej rodzina 
+  zostala zabita przez skorumpowanych policjantow.', 'Leon zawodowiec', 1994, 
+  110, 5);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(16, 'Cierpiaca na raka tarczycy Hazel za namowa rodzicow idzie na 
+  spotkanie grupy wsparcia. Poznaje tam nastoletniego Gusa, bylego koszykarza z 
+  amputowan¹ noga.' ,'Gwiazd naszych wina' , 2014, 125, 6);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(17, 'Los Angeles. Pianista jazzowy zakochuje sie w poczatkujacej 
+  aktorce.', 'La La Land', 2016, 126, 6);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(18, 'Bella gotowa jest zamieszkac w zamku potwora, aby ratowac ojca. Z 
+  czasem miedzy dziewczyna a bestia zaczyna rodzic sie uczucie. Jednoczesnie o 
+  reke Belli zabiega przystojny mysliwy Gaston.', 'Piekna i Bestia', 2017, 129, 
+  6);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(19, 'Po nieudanej ekspedycji Mark zostaje sam na Marsie. Mimo znikomych 
+  zapasow oraz zerwanej lacznosci z dowodztwem mezczyzna stara sie przetrwac w 
+  trudnych warunkach.', 'Marsjanin', 2015, 141, 7);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(20, 'Po serii ladowan Obcych na Ziemi wojsko werbuje ekspertke 
+  lingwistyki, ktorej zadaniem jest odkrycie nastawienia przybyszy z kosmosu.', 
+  'Nowy poczatek', 2016, 116, 7);  
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(21, 'Historia grupy rebeliantow zjednoczonych w smialej misji polegajacej 
+  na planów imperialnej Gwiazdy Smierci.', 'Lotr 1.', 
+  2016, 133, 7);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(22, 'Amerykanski agent specjalny pod przykrywk¹ skorumpowanego biznesmena 
+  usiluje rozpracowac organizacje przestepcza Pablo Escobara.', 'Boss', 2016, 
+  131, 8);
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(23, 'W Sudetach dochodzi do serii morderstw, ktorych ofiarami s¹ mysliwi. 
+  Wobec bezradnoœci policji œledztwo rozpoczyna emerytowana in¿ynierka', 
+  'Pokot', 2017, 128, 8);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(24, 'Dwoch policjantow stara sie zlapac seryjnego morderce wybierajacego 
+  swoje ofiary wg specjalnego "klucza".', 'Siedem', 1995, 127, 8);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(25, 'Christopher konczy studia, ale zamiast poswiecic sie karierze 
+  zawodowej, wyrusza autostopem w kierunku Alaski.', 'Wszystko za zycie', 2007, 
+  148, 9);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(26, 'Druzyna Pierscienia zostaje rozbita, lecz zdesperowany Frodo za 
+  wszelka cenê chce wype³nic powierzone mu zadanie.', 'Dwie wieze', 2002, 179, 
+  9);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(27, 'Aragorn jednoczy sily Srodziemia, szykujac sie do bitwy, ktora ma 
+  odwrocic uwage Saurona od podazajacych w kierunku Gory Przeznaczenia 
+  hobbitów.', 'Powrot krola', 2003, 201, 9);   
+  
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(28, 'Podczas drugiej wojny swiatowej Wladys³aw Szpilman, znakomity polski 
+  pianista, stara sie przezyc w okupowanej Warszawie.', 'Pianista', 2002, 120, 
+  10);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(29, 'Geniusz matematyczny John Nash za wszelka cene pragnie opracowac 
+  teorie, dzieki ktorej zostanie cenionym naukowcem. Przeszkoda staje sie jego 
+  stopniowo rozwijajaca choroba.', 'Piekny umysl', 2001, 135, 10);
+
+INSERT INTO Film(id, opis, tytul, rok_produkcji, dlugosc, gatunek_id)
+VALUES(30, 'Sparalizowany milioner zatrudnia do opieki mlodego chlopaka z 
+  przedmiescia, ktory wlasnie wyszedl z wiezienia.', 'Nietykalni', 2011, 142, 
+  10);
