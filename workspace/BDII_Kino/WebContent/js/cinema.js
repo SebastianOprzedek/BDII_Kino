83bb6c903c14708723a0e4d60387e210cd74992d
Łukasz Kozień (lukakoz566) <lukakoz566@student.polsl.pl>
2017-06-18 20:05:26 +0200

    popup

--- workspace/BDII_Kino/WebContent/js/cinema.js
+++ workspace/BDII_Kino/WebContent/js/cinema.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     updateTable();
 });
 
 function updateTable() {
     var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var films = JSON.parse(this.response)["films"];
             createTable(films);
@@ -15,31 +15,68 @@ function updateTable() {
     http.send();
 }
 
+function popup(film) {
+    var e = document.getElementById("popupBox");
+        e.style.display = 'block';
+
+    var content = "";
+
+    content += "<h1>" + film.title + "</h1>";
+    content += "<p> Gatunek: " + film.genre.name + "</p>";
+    content += "<p> Rok produkcji: " + film.production_year + "</p>";
+    content += "<p> Długość: " + film.length + "</p>";
+    content += "<p> Opis:<br> " + film.description + "</p>";
+    content += "<div class=\"btn-group\" role=\"group\" style=\"float:right\">";
+    content += "<a class=\"btn btn-secondary\" role=\"button\" href=\"javascript:void(0)\" onclick=\"closePopup();\">Przejdź do rezerwacji</a>"
+    content += "<a class=\"btn btn-secondary\" role=\"button\" href=\"javascript:void(0)\" onclick=\"closePopup();\">Zamknij</a></div>";
+    content += "<div class=\"clearfix\"/>";
+    document.getElementById("popupContent").innerHTML = content;
+
+
+
+}
+
+function closePopup() {
+    var e = document.getElementById("popupBox");
+  //  if(e.style.display == 'block')
+        e.style.display = 'none';
+   // else
+   //     e.style.display = 'block';
+}
+
 function createTable(films) {
     var table = document.getElementById("table"),
         tbl = document.createElement('table');
     tbl.style.width = '100%';
     tbl.classList.add('table');
     tbl.classList.add('table-condensed');
-    console.log(films.length);
+    // console.log(films.length);
     for (var i = 0; i < films.length; i += 3) {
         var tr = document.createElement('tr');
         for (var j = 0; j < 3; j++) {
             var td = document.createElement('td');
-            console.log(films[i + j].photos);
+            var link = document.createElement('a');
+            link.onclick = (function () {
+                var film = films[i + j];
+                return function () {
+                    popup(film);
+                }
+            })();
+            // console.log(films[i + j].photos);
             if (films[i + j].photos[0] != undefined) {
                 var imageContainer = document.createElement('div');
                 showPhoto(films[i + j].photos[0].idc, imageContainer);
-                td.appendChild(imageContainer);
+                link.appendChild(imageContainer);
             } else {
-                td.appendChild(document.createTextNode("BRAK OBRAZKA! :("));
+                link.appendChild(document.createTextNode("BRAK OBRAZKA! :("));
             }
-            td.appendChild(document.createElement('br'));
-            td.appendChild(document.createTextNode(films[i + j]["title"]));
+            link.appendChild(document.createElement('br'));
+            link.appendChild(document.createTextNode(films[i + j]["title"]));
+            td.appendChild(link);
             tr.appendChild(td);
         }
-        console.log(i);
-        console.log(tr);
+        //   console.log(i);
+        //   console.log(tr);
         tbl.appendChild(tr);
     }
     table.appendChild(tbl);
@@ -47,7 +84,7 @@ function createTable(films) {
 
 function showPhoto(photoId, photoContainer) {
     var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function () {
         if (this.readyState == 4) {
             var photo = document.createElement('img');
             photo.width = 200;
@@ -61,3 +98,4 @@ function showPhoto(photoId, photoContainer) {
     http.setRequestHeader("Content-type", "application/json");
     http.send();
 }
+
