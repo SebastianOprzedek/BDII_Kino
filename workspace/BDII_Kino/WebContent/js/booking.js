6e1f7134741976ab6019a3822b30220afd6e8f37
Łukasz Kozień (lukakoz566) <lukakoz566@student.polsl.pl>
2017-08-26 18:20:18 +0200

    wybor seansu - rezerwacje

--- workspace/BDII_Kino/WebContent/js/booking.js
+++ workspace/BDII_Kino/WebContent/js/booking.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     var url = new URL(window.location.href);
     var id = url.searchParams.get("id");
     updateTable(id);
@@ -6,21 +6,120 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function updateTable(id) {
     var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.response);
             var shows = JSON.parse(this.response)["shows"];
+            console.log(shows);
             if (shows.length == 0)
                 document.getElementById("table").innerHTML = "Lista seansów dla tego filmu jest pusta";
             else {
-                document.getElementById("table").innerHTML = "Dost&#281pne id sensów: <br>";
-                for (var i = 0; i < shows.length; i++) {
-                    document.getElementById("table").innerHTML = document.getElementById("table").innerHTML + shows[i].id + "<br>";
-                }
+                createTable(shows);
             }
         }
     };
     http.open("GET", "/cinema/rest/film/" + id + "/shows", true);
     http.setRequestHeader("Content-type", "application/json");
     http.send();
+}
+
+function createTable(shows) {
+    var table = document.getElementById("table");
+    var showsTable = document.createElement('table');
+
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
+
+    showsTable.style.width = '100%';
+    showsTable.classList.add('content');
+    showsTable.classList.add('table');
+    showsTable.classList.add('table-hover');
+    showsTable.classList.add('table-bordered');
+    showsTable.classList.add('text-center');
+    var thead = document.createElement('thead');
+    var tr = document.createElement('tr');
+    var th = document.createElement('th');
+    th.appendChild(document.createTextNode('Data'));
+    tr.appendChild(th);
+    th = document.createElement('th');
+    th.appendChild(document.createTextNode('Godzina'));
+    tr.appendChild(th);
+    th = document.createElement('th');
+    th.appendChild(document.createTextNode('Sala'));
+    tr.appendChild(th);
+    th = document.createElement('th');
+    th.appendChild(document.createTextNode('Rezerwuj'));
+    tr.appendChild(th);
+    thead.appendChild(tr)
+    showsTable.appendChild(thead);
+
+    var tbody = document.createElement('tbody');
+    for (var i = 0; i < shows.length; i++) {
+        var showDate = new Date(shows[i].data);
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(showDate.getUTCDate().toString() + "/" + (showDate.getUTCMonth() + 1).toString() + "/" + showDate.getUTCFullYear().toString()));
+        tr.appendChild(td);
+        td = document.createElement('td');
+        var hours = (showDate.getUTCHours() + 2).toString(); //+2 bo nie wiem czemu nie ogarnia strefy czasowej pacan jeden
+        var minutes;
+        if (showDate.getUTCMinutes() < 10)
+            minutes = "0" + showDate.getUTCMinutes().toString();
+        else
+            minutes = showDate.getUTCMinutes().toString();
+        var showTime = hours + ":" + minutes;
+        td.appendChild(document.createTextNode(showTime));
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.appendChild(document.createTextNode(shows[i].hall.name));
+        tr.appendChild(td);
+        td = document.createElement('td');
+        var button = document.createElement('button');
+        button.classList.add('btn');
+        button.classList.add('btn-primary');
+        button.classList.add('btn-sm');
+        button.type = "button";
+        button.onclick = (function () {
+            var show = shows[i];
+            return function () {
+                popup(show);
+            }
+        })();
+        var t = document.createTextNode("Wybierz");
+        button.appendChild(t);
+        td.appendChild(button);
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+    }
+    showsTable.appendChild(tbody);
+    table.appendChild(showsTable);
+}
+
+function popup(show) {
+    console.log(show);
+    document.getElementById("popupBox").style.display = 'block';
+    var showDate = new Date(show.data);
+    var showDay = showDate.getUTCDate().toString();
+    var showMonth = (showDate.getUTCMonth() + 1).toString();
+    var showYear = showDate.getUTCFullYear().toString();
+    var showHours = (showDate.getUTCHours() + 2).toString(); //+2 bo nie wiem czemu nie ogarnia strefy czasowej pacan jeden
+    var showMinutes;
+    if (showDate.getUTCMinutes() < 10)
+        showMinutes = "0" + showDate.getUTCMinutes().toString();
+    else
+        showMinutes = showDate.getUTCMinutes().toString();
+    var showDateString = showDay + "/" + showMonth + "/" + showYear + "  " + showHours + ":" + showMinutes;
+    var content = "";
+    content += "<h1>" + show.film.title ;
+    content += "<div class=\"btn-group\" role=\"group\" style=\"float:right\">";
+    content += "<a class=\"btn btn-secondary btn-sm\" role=\"button\" onclick=\"closePopup();\">X</a></div></h1>";
+    content += "<b>" + showDateString + "</b>"
+    content += "<h2>Wybierz miejsca</h2>";
+    content += "<p>Tu jakos bedzie wybor miejsc</p>";
+    content += "<div class=\"clearfix\"/>";
+    document.getElementById("popupContent").innerHTML = content;
+}
+
+function closePopup() {
+    document.getElementById("popupBox").style.display = 'none';
 }
--- workspace/BDII_Kino/WebContent/js/films.js
+++ workspace/BDII_Kino/WebContent/js/films.js
@@ -1,9 +1,9 @@
 var filmObject;
-document.addEventListener("DOMContentLoaded", function(event) {
-  updateTable();
-  document.getElementById("addFilm").style.display = "none";
-  document.getElementById("updateFilm").style.display = "none";
-  document.getElementById("photos").style.display = "none";
+document.addEventListener("DOMContentLoaded", function (event) {
+    updateTable();
+    document.getElementById("addFilm").style.display = "none";
+    document.getElementById("updateFilm").style.display = "none";
+    document.getElementById("photos").style.display = "none";
 });
 
 function readImage(input) {
@@ -16,146 +16,147 @@ function readImage(input) {
     }
 }
 
-function addImage(image){
-  $('html, body').animate({ scrollTop: 0 }, 'fast');
-  image = image.replace('data:image/png;base64,','')
-  image = image.replace('data:image/jpeg;base64,','')
+function addImage(image) {
+    $('html, body').animate({scrollTop: 0}, 'fast');
+    image = image.replace('data:image/png;base64,', '')
+    image = image.replace('data:image/jpeg;base64,', '')
     var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200){
-        updateTable();
-      }
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            updateTable();
+        }
     };
     http.open("POST", "/cinema/rest/photo/" + filmId, true);
     http.setRequestHeader("Content-Type", "application/json");
     http.send(image);
     document.getElementById("addFilm").style.display = "none";
     document.getElementById("updateFilm").style.display = "none";
-	document.getElementById("photos").style.display = "none";
+    document.getElementById("photos").style.display = "none";
 }
 
 function deleteImage(id) {
-  var http = new XMLHttpRequest();
-  http.open("DELETE", "/cinema/rest/photo/" + id, true);
-  http.send();
+    var http = new XMLHttpRequest();
+    http.open("DELETE", "/cinema/rest/photo/" + id, true);
+    http.send();
 }
 
 function checkAddData() {
-	if ($('#addFilmDesc').val() == '' || $('#addFilmTitle').val() == ''
-		|| $('#addFilmYear').val() == '' || $('#addFilmLength').val() == '' || $('#addFilmGenre').val() == ''){
-		alert("Dane niekompletne");
-		return false;
-	} 
-	else if (genre($('#addFilmGenre').val()) == -1){
-		alert("Gatunek niepoprawny");
-		return false;
-	}
-	else if(isNaN($('#addFilmYear').val())){
-		alert("Rok musi byc wartoscia liczbowa");
-		return false;
-	}
-	else if(isNaN($('#addFilmLength').val())){
-		alert("Dlugosc filmu musi byc wartoscia liczbowa (czas w minutach)");
-		return false;
-	}
-	
-	return true;
+    if ($('#addFilmDesc').val() == '' || $('#addFilmTitle').val() == ''
+        || $('#addFilmYear').val() == '' || $('#addFilmLength').val() == '' || $('#addFilmGenre').val() == '') {
+        alert("Dane niekompletne");
+        return false;
+    }
+    else if (genre($('#addFilmGenre').val()) == -1) {
+        alert("Gatunek niepoprawny");
+        return false;
+    }
+    else if (isNaN($('#addFilmYear').val())) {
+        alert("Rok musi byc wartoscia liczbowa");
+        return false;
+    }
+    else if (isNaN($('#addFilmLength').val())) {
+        alert("Dlugosc filmu musi byc wartoscia liczbowa (czas w minutach)");
+        return false;
+    }
+
+    return true;
 }
 
 function checkUpdateData() {
-	if ($('#updateFilmDesc').val() == '' || $('#updateFilmTitle').val() == ''
-		|| $('#updateFilmYear').val() == '' || $('#updateFilmLength').val() == '' || $('#updateFilmGenre').val() == ''){
-		alert("Dane niekompletne");
-		return false;
-	} 
-	else if (genre($('#updateFilmGenre').val()) == -1){
-		alert("Gatunek niepoprawny");
-		return false;
-	}
-	else if(isNaN($('#updateFilmYear').val())){
-		alert("Rok musi byc wartoscia liczbowa");
-		return false;
-	}
-	else if(isNaN($('#updateFilmLength').val())){
-		alert("Dlugosc filmu musi byc wartoscia liczbowa (czas w minutach)");
-		return false;
-	}
-	
-	return true;
+    if ($('#updateFilmDesc').val() == '' || $('#updateFilmTitle').val() == ''
+        || $('#updateFilmYear').val() == '' || $('#updateFilmLength').val() == '' || $('#updateFilmGenre').val() == '') {
+        alert("Dane niekompletne");
+        return false;
+    }
+    else if (genre($('#updateFilmGenre').val()) == -1) {
+        alert("Gatunek niepoprawny");
+        return false;
+    }
+    else if (isNaN($('#updateFilmYear').val())) {
+        alert("Rok musi byc wartoscia liczbowa");
+        return false;
+    }
+    else if (isNaN($('#updateFilmLength').val())) {
+        alert("Dlugosc filmu musi byc wartoscia liczbowa (czas w minutach)");
+        return false;
+    }
+
+    return true;
 }
 
 function addFilm() {
-  $('html, body').animate({ scrollTop: 0 }, 'fast');
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200)
-		  updateTable();
-  };
-  http.open("POST", "/cinema/rest/film", true);
-  http.setRequestHeader("Content-Type", "application/json");
-  var film = new Object();
-  var correctFormat = checkAddData();
-  
-  if(correctFormat) {
-    film.description = $('#addFilmDesc').val();
-    film.title = $('#addFilmTitle').val();
-    film.production_year = parseFloat($('#addFilmYear').val());
-    film.length = parseFloat($('#addFilmLength').val());
-    film.genre = genre($('#addFilmGenre').val());
-    http.send(JSON.stringify(film));
-	alert("Dodano film");
-  }
+    $('html, body').animate({scrollTop: 0}, 'fast');
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200)
+            updateTable();
+    };
+    http.open("POST", "/cinema/rest/film", true);
+    http.setRequestHeader("Content-Type", "application/json");
+    var film = new Object();
+    var correctFormat = checkAddData();
+
+    if (correctFormat) {
+        film.description = $('#addFilmDesc').val();
+        film.title = $('#addFilmTitle').val();
+        film.production_year = parseFloat($('#addFilmYear').val());
+        film.length = parseFloat($('#addFilmLength').val());
+        film.genre = genre($('#addFilmGenre').val());
+        http.send(JSON.stringify(film));
+        alert("Dodano film");
+    }
 }
 
 function updateFilm() {
-  $('html, body').animate({ scrollTop: 0 }, 'fast');
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200)
-		  updateTable();
-  };
-  http.open("PUT", "/cinema/rest/film/" + filmObject.id, true);
-  http.setRequestHeader("Content-Type", "application/json");
-  var film = new Object();
-  var correctFormat = checkUpdateData();
-  
-  if(correctFormat) {
-    film.id = filmObject.id;
-    film.photos = filmObject.photos;
-    film.title = $('#updateFilmTitle').val();
-    film.description = $('#updateFilmDesc').val();
-    film.production_year = parseFloat($('#updateFilmYear').val());
-    film.length = parseFloat($('#updateFilmLength').val());
-    film.genre = genre($('#updateFilmGenre').val());
-    http.send(JSON.stringify(film));
-	alert("Zaktualizowano");
-  }
+    $('html, body').animate({scrollTop: 0}, 'fast');
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200)
+            updateTable();
+    };
+    http.open("PUT", "/cinema/rest/film/" + filmObject.id, true);
+    http.setRequestHeader("Content-Type", "application/json");
+    var film = new Object();
+    var correctFormat = checkUpdateData();
+
+    if (correctFormat) {
+        film.id = filmObject.id;
+        film.photos = filmObject.photos;
+        film.title = $('#updateFilmTitle').val();
+        film.description = $('#updateFilmDesc').val();
+        film.production_year = parseFloat($('#updateFilmYear').val());
+        film.length = parseFloat($('#updateFilmLength').val());
+        film.genre = genre($('#updateFilmGenre').val());
+        http.send(JSON.stringify(film));
+        alert("Zaktualizowano");
+    }
 }
 
-function update(film){
-	$('html, body').animate({ scrollTop: 0 }, 'fast');
-  filmObject= film;
-	document.getElementById("addFilm").style.display = "none";
-  document.getElementById("updateFilm").style.display = "block";
-  document.getElementById("photos").style.display = "none";
-	document.getElementById("updateFilmTitle").value = film.title;
-	document.getElementById("updateFilmDesc").value = film.description;
-	document.getElementById("updateFilmYear").value = film.production_year;
-	document.getElementById("updateFilmLength").value = film.length;
-	document.getElementById("updateFilmGenre").value = film.genre.name;
-}
-
-function showAddFilm(){
-  	document.getElementById("addFilm").style.display = "block";
-  	document.getElementById("updateFilm").style.display = "none";
+function update(film) {
+    $('html, body').animate({scrollTop: 0}, 'fast');
+    filmObject = film;
+    document.getElementById("addFilm").style.display = "none";
+    document.getElementById("updateFilm").style.display = "block";
     document.getElementById("photos").style.display = "none";
- 		document.getElementById("addFilmTitle").value = "";
- 		document.getElementById("addFilmDesc").value = "";
- 		document.getElementById("addFilmYear").value = "";
- 		document.getElementById("addFilmLength").value = "";
- 		document.getElementById("addFilmGenre").value = "";
+    document.getElementById("updateFilmTitle").value = film.title;
+    document.getElementById("updateFilmDesc").value = film.description;
+    document.getElementById("updateFilmYear").value = film.production_year;
+    document.getElementById("updateFilmLength").value = film.length;
+    document.getElementById("updateFilmGenre").value = film.genre.name;
 }
-function showPhotos(film){
+
+function showAddFilm() {
+    document.getElementById("addFilm").style.display = "block";
+    document.getElementById("updateFilm").style.display = "none";
+    document.getElementById("photos").style.display = "none";
+    document.getElementById("addFilmTitle").value = "";
+    document.getElementById("addFilmDesc").value = "";
+    document.getElementById("addFilmYear").value = "";
+    document.getElementById("addFilmLength").value = "";
+    document.getElementById("addFilmGenre").value = "";
+}
+
+function showPhotos(film) {
     filmId = film.id;
     document.getElementById("addFilm").style.display = "none";
     document.getElementById("updateFilm").style.display = "none";
@@ -166,79 +167,79 @@ function showPhotos(film){
     }
     photosContainer.style.display = "inline";
     var photos = film.photos;
-    for(var i=0; i<photos.length; i++){
-      var http = new XMLHttpRequest();
-      http.onreadystatechange = (function() {
-        var photoObject = photos[i];
-        var filmObject = film;
-        return function() {
-        if (this.readyState == 4){
-          var photoDiv = document.createElement("div");
-          var photo = document.createElement('img');
-          photo.width=200;
-          photo.height=289;
-          var image = "data:image/png;base64," + this.response;
-         	photo.setAttribute("src", image);
-          var icon = document.createElement('i');
-          icon.classList.add('fa');
-          icon.classList.add('fa-trash');
-          icon.classList.add('icons-margin');
-          icon.classList.add('fa-5x');
-          icon.onclick = (function(){
-              var film = filmObject;
-              var index = filmObject.photos.indexOf(photoObject);
-              filmObject.photos.splice(index, 1);
-              var idc = photoObject["idc"];
-              return function() {
-               deleteImage(idc);
-           		 updateTable();
-               document.getElementById("addFilm").style.display = "none";
-               document.getElementById("updateFilm").style.display = "none";
-			   document.getElementById("photos").style.display = "none";
-             }
-          })();
-          photoDiv.appendChild(photo);
-          photoDiv.appendChild(icon);
-          photosContainer.appendChild(photoDiv);
-        }
-      }
-      })();
-      http.open("GET", "/cinema/rest/photo/"+photos[i].idc, true);
-      http.setRequestHeader("Content-type", "application/json");
-      http.send();
+    for (var i = 0; i < photos.length; i++) {
+        var http = new XMLHttpRequest();
+        http.onreadystatechange = (function () {
+            var photoObject = photos[i];
+            var filmObject = film;
+            return function () {
+                if (this.readyState == 4) {
+                    var photoDiv = document.createElement("div");
+                    var photo = document.createElement('img');
+                    photo.width = 200;
+                    photo.height = 289;
+                    var image = "data:image/png;base64," + this.response;
+                    photo.setAttribute("src", image);
+                    var icon = document.createElement('i');
+                    icon.classList.add('fa');
+                    icon.classList.add('fa-trash');
+                    icon.classList.add('icons-margin');
+                    icon.classList.add('fa-5x');
+                    icon.onclick = (function () {
+                        var film = filmObject;
+                        var index = filmObject.photos.indexOf(photoObject);
+                        filmObject.photos.splice(index, 1);
+                        var idc = photoObject["idc"];
+                        return function () {
+                            deleteImage(idc);
+                            updateTable();
+                            document.getElementById("addFilm").style.display = "none";
+                            document.getElementById("updateFilm").style.display = "none";
+                            document.getElementById("photos").style.display = "none";
+                        }
+                    })();
+                    photoDiv.appendChild(photo);
+                    photoDiv.appendChild(icon);
+                    photosContainer.appendChild(photoDiv);
+                }
+            }
+        })();
+        http.open("GET", "/cinema/rest/photo/" + photos[i].idc, true);
+        http.setRequestHeader("Content-type", "application/json");
+        http.send();
     }
 }
 
 // DELETE BY ID
 function deleteById(id) {
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-		updateTable();
-  };
-  http.open("DELETE", "/cinema/rest/film/" + id, true);
-  http.send();
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        updateTable();
+    };
+    http.open("DELETE", "/cinema/rest/film/" + id, true);
+    http.send();
 }
 
 // TABELA NA GÓRZE
 function updateTable() {
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
-      tableCreate(JSON.parse(this.response)["films"]);
-     }
-  };
-  http.open("GET", "/cinema/rest/film", true);
-  http.setRequestHeader("Content-type", "application/json");
-  http.send();
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            tableCreate(JSON.parse(this.response)["films"]);
+        }
+    };
+    http.open("GET", "/cinema/rest/film", true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send();
 }
 
-function tableCreate(films){
+function tableCreate(films) {
     var table = document.getElementById("table"),
-        tbl  = document.createElement('table');
+        tbl = document.createElement('table');
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
-    tbl.style.width  = '100%';
+    tbl.style.width = '100%';
     tbl.classList.add('table');
     tbl.classList.add('table-condensed');
 
@@ -263,13 +264,15 @@ function tableCreate(films){
     icon.classList.add('fa');
     icon.classList.add('fa-plus');
     icon.classList.add('icons-margin');
-    icon.onclick = function(){showAddFilm()};
-	$('html, body').animate({ scrollTop: 0 }, 'fast');
+    icon.onclick = function () {
+        showAddFilm()
+    };
+    $('html, body').animate({scrollTop: 0}, 'fast');
     td.appendChild(icon);
     tr.appendChild(td);
     tbl.appendChild(tr);
 
-    for(var i = 0; i < films.length; i++){
+    for (var i = 0; i < films.length; i++) {
         var tr = document.createElement('tr');
         var td = document.createElement('td');
         td.appendChild(document.createTextNode(films[i]["title"]));
@@ -291,37 +294,37 @@ function tableCreate(films){
         icon.classList.add('fa');
         icon.classList.add('fa-pencil');
         icon.classList.add('icons-margin');
-        icon.onclick = (function(){
+        icon.onclick = (function () {
             var film = films[i];
-            return function() {
-             update(film);
-           }
+            return function () {
+                update(film);
+            }
         })();
         td.appendChild(icon);
         var icon = document.createElement('i');
         icon.classList.add('fa');
         icon.classList.add('fa-trash');
         icon.classList.add('icons-margin');
-        icon.onclick = (function(){
+        icon.onclick = (function () {
             var id = films[i]["id"];
-            return function() {
-             deleteById(id);
-			 document.getElementById("addFilm").style.display = "none";
-			 document.getElementById("updateFilm").style.display = "none";
-  			 document.getElementById("photos").style.display = "none";
-           }
+            return function () {
+                deleteById(id);
+                document.getElementById("addFilm").style.display = "none";
+                document.getElementById("updateFilm").style.display = "none";
+                document.getElementById("photos").style.display = "none";
+            }
         })();
         td.appendChild(icon);
         var icon = document.createElement('i');
         icon.classList.add('fa');
         icon.classList.add('fa-picture-o');
         icon.classList.add('icons-margin');
-        icon.onclick = (function(){
+        icon.onclick = (function () {
             var film = films[i];
-            return function() {
-             showPhotos(film);
-			 $('html, body').animate({ scrollTop: 0 }, 'fast');
-           }
+            return function () {
+                showPhotos(film);
+                $('html, body').animate({scrollTop: 0}, 'fast');
+            }
         })();
         td.appendChild(icon);
         tr.appendChild(td);
@@ -331,19 +334,20 @@ function tableCreate(films){
 }
 
 function genre(genreName) {
-  var genre = -1;
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-          var genres = JSON.parse(this.response)["genres"];
-          for(var i=0;i<genres.length;i++){
-          if (genres[i]["name"] == genreName)
-            genre = genres[i];
-          }
-    };
-  }
-  http.open("GET", "/cinema/rest/genre", false);
-  http.setRequestHeader("Content-type", "application/json");
-  http.send();
-  return genre;
+    var genre = -1;
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var genres = JSON.parse(this.response)["genres"];
+            for (var i = 0; i < genres.length; i++) {
+                if (genres[i]["name"] == genreName)
+                    genre = genres[i];
+            }
+        }
+        ;
+    }
+    http.open("GET", "/cinema/rest/genre", false);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send();
+    return genre;
 }
